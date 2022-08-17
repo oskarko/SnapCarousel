599484795e502cb492cdb55dd613d56ff520d33f
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2022-08-17 20:45:12 +0200

    Initial Commit

---
+++ SwiftUICarousel/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUICarousel
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2022 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICarousel/SwiftUICarouselApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUICarouselApp.swift
+//  SwiftUICarousel
+//
+//  Created by Oscar R. Garrucho.
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2022 Oscar R. Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
